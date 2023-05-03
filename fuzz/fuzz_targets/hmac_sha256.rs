2380f2c7fb9814a29ad224e33ae18469e18a84d7
Matthew Jurenka <matt.jurenka@comcast.net>
2023-05-03 23:10:56 +0000

    added mayhem fuzzing for sha hmac

---
+++ fuzz/fuzz_targets/hmac_sha256.rs
@@ -0,0 +1,15 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
+
+// TODO: add docs about arbitrary
+fuzz_target!(|input: (&[u8], &[u8])| {
+    let (key, data) = input;
+    type HmacSha256 = Hmac<Sha256>;
+    let mut mac = HmacSha256::new_from_slice(key)
+        .expect("HMAC can take key of any size");
+    mac.update(data);
+    let result = mac.clone().finalize().into_bytes();
+    mac.verify_slice(&result).expect("Failed to verify message");
+});
